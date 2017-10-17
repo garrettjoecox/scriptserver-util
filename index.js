2f469cd9c15903e25bc9f9282db7a7d5d83d75ea
Garrett Cox <garrettjcox@gmail.com>
2017-10-16 19:00:11 -0500

    Update for 1.0.0

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-'use strict';
 
 const fs = require('fs');
-const path = require('path');
+const { join } = require('path');
+const { promisify } = require('util');
 
-module.exports = function() {
-
+module.exports = function () {
   const server = this;
 
   server.util = {
@@ -16,55 +15,61 @@ module.exports = function() {
       if (typeof player !== 'string') return Promise.reject(new Error('util.isOp: Player name needs to be a string'));
 
       return readFile('ops.json')
-        .then(ops => !!JSON.parse(ops).filter(op => op.name === player).length);
+        .then(ops => JSON.parse(ops))
+        .then(ops => !!ops.filter(op => op.name === player).length);
     },
 
     isOnline(player) {
       if (!player) return Promise.reject(new Error('util.isOnline: Please provide player name'));
       if (typeof player !== 'string') return Promise.reject(new Error('util.isOnline: Player name needs to be a string'));
 
-      return server.send(`testfor ${player}`, /Found\s([\w]+)/, /cannot be found/)
-        .then(() => true)
-        .catch(() => false);
+      return server.send(`testfor ${player}`)
+        .then(result => !!result.match(/^Found\s(\w+)$/));
     },
 
-    getLocation(player) {
-      if (!player) return Promise.reject(new Error('util.getLocation: Please provide player name'));
-      if (typeof player !== 'string') return Promise.reject(new Error('util.getLocation: Player name needs to be a string'));
-      const location = {};
+    async getDimension(player) {
+      if (!player) throw new Error('util.getDimension: Please provide player name');
+      if (typeof player !== 'string') throw new Error('util.getDimension: Player name needs to be a string');
+      if (!await this.isOnline(player)) throw new Error(`util.getDimension: ${player} is not online`);
 
-      return this.isOnline(player)
-        .then(isOnline => isOnline ? null : Promise.reject(new Error(`util.getLocation: ${player} is not online`)))
-        .then(() => {
-          return server.send(`testfor ${player} {Dimension:0}`, /Found\s([\w]+)/, /did\snot\smatch/)
-            .then(() => location.dimension = 'overworld')
-            .catch(() => {
-              return server.send(`testfor ${player} {Dimension:-1}`, /Found\s([\w]+)/, /did\snot\smatch/)
-                .then(() => location.dimension = 'nether')
-                .catch(() => {
-                  return server.send(`testfor ${player} {Dimension:1}`, /Found\s([\w]+)/, /did\snot\smatch/)
-                    .then(() => location.dimension = 'end')
-                    .catch(() => Promise.reject(new Error(`util.getLocation: ${player} in unknown dimension`)));
-                });
-            });
-        })
-        .then(() => server.send(`execute ${player} ~ ~ ~ /testforblock ~ ~ ~ minecraft:air 10`, /at\s([-\d]+), ([-\d]+), ([-\d]+)/))
-        .then(data => {
-          if (!data) return Promise.reject(new Error('util.getLocation: Unable to retrieve coordinates'));
-          else {
-            location.x = data[1];
-            location.y = data[2];
-            location.z = data[3];
-          }
-        })
-        .then(() => location);
+      const overworldResult = await server.send(`testfor ${player} {Dimension:0}`);
+      const netherResult = await server.send(`testfor ${player} {Dimension:-1}`);
+      const endResult = await server.send(`testfor ${player} {Dimension:1}`);
+      const foundRegex = /^Found\s(\w+)$/;
+
+      if (overworldResult.match(foundRegex)) {
+        return 'overworld';
+      } else if (netherResult.match(foundRegex)) {
+        return 'nether';
+      } else if (endResult.match(foundRegex)) {
+        return 'end';
+      }
+
+      throw new Error(`util.getDimension: ${player} in unkown dimension`);
+    },
+
+    async getLocation(player) {
+      if (!player) throw new Error('util.getLocation: Please provide player name');
+      if (typeof player !== 'string') throw new Error('util.getLocation: Player name needs to be a string');
+      if (!await this.isOnline(player)) throw new Error(`util.getLocation: ${player} is not online`);
+
+      const result = await server.send(`execute ${player} ~ ~ ~ /testforblock ~ ~ ~ minecraft:air 10`);
+      const coords = result.match(/at\s([-\d]+), ([-\d]+), ([-\d]+)/);
+
+      return {
+        dimension: await this.getDimension(player),
+        x: coords[1],
+        y: coords[2],
+        z: coords[3],
+      };
     },
 
     // General utilities
 
     getOnlineAmount() {
-      return server.send('list', /]: There are ([\d]+)\/([\d]+)/)
-        .then(result => parseInt(result[1]));
+      return server.send('list')
+        .then(result => result.match(/There are ([\d]+)\/([\d]+)/))
+        .then(result => parseInt(result[1], 10));
     },
 
     tellRaw(message, target = '@a', options = {}) {
@@ -77,18 +82,12 @@ module.exports = function() {
 
     wait(time) {
       return new Promise(resolve => setTimeout(resolve, time));
-    }
-
+    },
   };
-}
+};
 
 // Helper Functions
 
 function readFile(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join(process.cwd(), file), 'utf8', (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+  return promisify(fs.readFile)(join(process.cwd(), file), 'utf8');
 }
